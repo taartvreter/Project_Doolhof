b67aa98a7a1834b2e35ab5b1d4aa94aa01a1d629
Huub van Holsteijn <hwpvanholsteijn@gmail.com>
2017-03-30 13:25:01 +0200

    as
    PackageNames eddited

--- src/data/model/Key.java
+++ src/data/model/Key.java
@@ -3,11 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-<<<<<<< HEAD
+
 package data.model;
-=======
-package data.models;
->>>>>>> master
 
 /**
  *

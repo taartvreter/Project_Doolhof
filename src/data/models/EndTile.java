2d8ebe1d7fd8893dfbe54f9b8b4a79895d8dc9fa
Huub van Holsteijn <hwpvanholsteijn@gmail.com>
2017-03-30 12:08:26 +0200

    Added SomeModels

---
+++ src/data/models/EndTile.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author hwpva
+ */
+public class EndTile extends Tile{
+    public void showWinningMessage(){
+        javax.swing.JOptionPane.showMessageDialog(new java.awt.Frame(),"Winnaar die je bent ;)!");
+    }
+}

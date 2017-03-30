fc4551cb69b81e367fc13c87268b4d619706954d
Jimballoons <jimvanlienden@Hotmail.com>
2017-03-30 21:26:12 +0200

    niet veel veranderd.

--- src/data/Launcher.java
+++ src/data/Launcher.java
@@ -12,7 +12,7 @@ package data;
 public class Launcher {
     
     public static void main(String[] args) throws Exception {
-        Game game = new Game("Project", 800, 800);
+        Game game = new Game("Project", 700, 700);
         game.start();
     }
 }

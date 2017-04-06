d0c38e7a295e9a252fc0bc34138f81a251a81616
Jimballoons <jimvanlienden@Hotmail.com>
2017-04-06 15:40:17 +0200

    ajksdba

--- test/data/GameTest.java
+++ test/data/GameTest.java
@@ -21,62 +21,7 @@ public class GameTest {
     
     public GameTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of run method, of class Game.
-     */
-    @Test
-    public void testRun() {
-        System.out.println("run");
-        Game instance = null;
-        instance.run();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of start method, of class Game.
-     */
-    @Test
-    public void testStart() {
-        System.out.println("start");
-        Game instance = null;
-        instance.start();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of stop method, of class Game.
-     */
-    @Test
-    public void testStop() {
-        System.out.println("stop");
-        Game instance = null;
-        instance.stop();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of tryPlayerMove method, of class Game.
-     */
     @Test
     public void testTryPlayerMove() {
         System.out.println("tryPlayerMove");
